3cff4d876925d51ab36b2585a0acecea814cab94
faisal hanip <faisalhanip123@gmail.com>
2026-02-07 14:23:09 +0700

    lanjut

--- tests/testselenium.py
+++ tests/testselenium.py
@@ -10,8 +10,6 @@ driver = webdriver.Chrome()
 # URL dasar
 url = "https://faisalhanip.github.io/automation/"
 
-driver.maximize_window()
-
 def buka_login_page():
     driver.get(url)
     time.sleep(2)
@@ -24,9 +22,8 @@ def test_login_sukses():
     driver.find_element(By.TAG_NAME, "button").click()
     time.sleep(2)
     assert "dashboard" in driver.current_url or "logout" in driver.page_source
-    print("✅ Login sukses - PASS")
 
-def test_login_username_salah():
+'''def test_login_username_salah():
     buka_login_page()
     driver.find_element(By.ID, "username").send_keys("salah")
     driver.find_element(By.ID, "password").send_keys("test")
@@ -44,7 +41,7 @@ def test_login_password_salah():
     assert "salah" in driver.page_source.lower() or "gagal" in driver.page_source.lower()
     print("✅ Login gagal (password salah) - PASS")
 
-def test_login_field_kosong():
+'''def test_login_field_kosong():
     buka_login_page()
     driver.find_element(By.ID, "username").send_keys("")
     driver.find_element(By.ID, "password").send_keys("")
@@ -69,4 +66,4 @@ test_login_field_kosong()
 test_logout()
 
 # Tutup browser
-driver.quit()
+driver.quit()'''
